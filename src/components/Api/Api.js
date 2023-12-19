ec6cca249b1a3b6e540f862ebc620ef70c995e1e
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-12-19 13:10:33 +0100

    axios add params

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -6,30 +6,37 @@ const options = {
     Authorization:
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwM2ExZGQxNDMyZGY5YjY4NmQzMDYzNmIxYmZlZGVmNiIsInN1YiI6IjY1NzU4ZTQ4YzYwMDZkMDExZjBkOTRhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EugJ-N1349adCMLGTH7afk_3FlznGp4An5rFeQfUHL4',
   },
+  params: {
+    language: 'en-US',
+  },
 };
 
 export const getTrendingMovies = async () => {
-  const url = `trending/movie/day?language=en-US`;
+  const url = `trending/movie/day`; // Tu jest ścieżka do zasobu
   const resp = await axios.get(url, options);
   return resp;
 };
+
 export const getMovies = async query => {
-  const url = `search/movie?query=${query}&include_adult=false&language=en-US`;
+  const url = `search/movie?query=${query}&include_adult=false`;
   const resp = await axios.get(url, options);
   return resp;
 };
+
 export const getMovieDetails = async id => {
-  const url = `movie/${id}?language=en-US`;
+  const url = `movie/${id}`;
   const resp = await axios.get(url, options);
   return resp;
 };
+
 export const getMovieCredits = async id => {
-  const url = `movie/${id}/credits?language=en-US`;
+  const url = `movie/${id}/credits`;
   const resp = await axios.get(url, options);
   return resp;
 };
+
 export const getMovieReviews = async id => {
-  const url = `movie/${id}/reviews?language=en-US&page=1`;
+  const url = `movie/${id}/reviews?page=1`;
   const resp = await axios.get(url, options);
   return resp;
 };
